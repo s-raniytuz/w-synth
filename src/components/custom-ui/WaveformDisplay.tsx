5cc776efb3bfb9d8416c410fcfeb709d39ccdfc4
s-raniytuz <tuzsocial909@gmail.com>
2024-08-11 15:45:10 -0600

    Add waveform images

--- src/components/custom-ui/WaveformDisplay.tsx
+++ src/components/custom-ui/WaveformDisplay.tsx
@@ -1,13 +1,44 @@
+import { useAppSelector } from "@/store/hooks";
 import WaveformSelector from "../WaveformSelector";
+import { cn } from "@/lib/utils";
 
 export default function WaveformDisplay() {
+  const waveform = useAppSelector((state) => state.synthOneWaveform.type);
+  const staticImageClassName = "waveform-image h-[87%] w-full";
+  let imageClassName;
+
+  switch (waveform) {
+    case "sine":
+      imageClassName = cn(staticImageClassName, 'bg-[url("/assets/sine.png")]');
+      break;
+    case "triangle":
+      imageClassName = cn(
+        staticImageClassName,
+        'bg-[url("/assets/triangle.png")]',
+      );
+      break;
+    case "sawtooth":
+      imageClassName = cn(
+        staticImageClassName,
+        'bg-[url("/assets/sawtooth.png")]',
+      );
+      break;
+    case "square":
+      imageClassName = cn(
+        staticImageClassName,
+        'bg-[url("/assets/square.png")]',
+      );
+      break;
+  }
+
   return (
-    <div className="waveform-display bg-centauri-black h-full w-full rounded-xl">
-      <div className="waveform-display-selector mt-px flex h-[15%] w-full items-center justify-between px-4">
+    <div className="waveform-display h-full w-full rounded-xl bg-centauri-black">
+      <div className="waveform-display-selector mt-px flex h-[13%] w-full items-center justify-between px-4">
         <div className="synth-switch h-[1rem] w-[1rem] rounded-[50%] bg-white"></div>
         <WaveformSelector synthId={1} className="w-[12rem]" />
         <div className="synth-switch h-[1rem] w-[1rem] rounded-[50%] bg-white opacity-0"></div>
       </div>
+      <div className={imageClassName}></div>
     </div>
   );
 }
